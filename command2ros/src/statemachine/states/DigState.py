ec9d3a0cacdf673f63c4f25bf01e267b1ea081e9
Emily Peterson <emilyrp15@gmail.com>
2018-03-26 18:33:23 -0700

    Update DigState.py

--- command2ros/src/statemachine/states/DigState.py
+++ command2ros/src/statemachine/states/DigState.py
@@ -4,7 +4,11 @@ class DigState(State):
     #init attributes of state
     def __init__(self):
         super().__init__("Dig", "ScanDump")
+        
 
     #implementation for each state: overridden
-    def run(self):
-        print("\n>run() not implemented\n")
+    def run(self, cr):
+        #new movement data with command dig set to true
+        dig_data = MovementData()
+        dig_data.dig = True
+        cr.setCommand(dig_data)
