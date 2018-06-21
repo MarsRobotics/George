5b44c02fefa2e059de3191a5e9265f6b6dd4575f
Lexden12 <lexden.s@gmail.com>
2018-06-20 22:28:23 -0700

    Minor Update
    Added third camera back in and fixed some comments.

--- command2ros/src/statemachine/cam_vision.py
+++ command2ros/src/statemachine/cam_vision.py
@@ -1,11 +1,11 @@
 import cv2
 
-cam=0#TODO get cam int from ROS
+cam=0#TODO get cam int from socket
 
 def videoSteam():
     frontCam = cv2.VideoCapture(0)#camera for maneuvering the field
     digCam = cv2.VideoCapture(1)#camera to tell how digging is working
-    #backCam = cv2.VideoCapture(2)#camera for backing up and docking to hopper
+    backCam = cv2.VideoCapture(2)#camera for backing up and docking to hopper
     while True:
         ret = None
         frame = None
@@ -15,7 +15,7 @@ def videoSteam():
             ret, frame = digCam.read()
         else:            
             ret, frame = frontCam.read()#default is the front 
-        cv2.imshow('frame', frame)#TODO send frame over ROS and display on laptop instead        
+        cv2.imshow('frame', frame)#TODO send frame over socket and display on laptop instead        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break#remove this if statement when displaying on laptop
     cv2.release()
